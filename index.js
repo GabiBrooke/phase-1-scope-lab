5b02b640bef8e7b0d2d7653ef97fbcec620c3c9f
git <gabbybailu@gmail.com>
2023-09-29 15:28:11 -0600

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your solution in this file!
+var customerName = 'bob';
+//var bestCustomer = 'not bob';
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+}
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+const leastFavoriteCustomer = 'Alec'
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'Gabi'
+}
